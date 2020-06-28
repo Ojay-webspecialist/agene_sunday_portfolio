a1f04e44570dd0492da81d257f996b250c0aa191
Sunday Agene <51954098+Ojay-webspecialist@users.noreply.github.com>
2020-06-28 23:50:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# agene_sunday_portfolio
+ Agene Sunday Ojima Portfolio built with Html,css3 ,Bootstrap 4 and Javascript
---
+++ index.js
@@ -0,0 +1,14 @@
+
+import getSharingImage from '@jlengstorf/get-share-image'
+const socialImage = getSharingImage({
+  title: 'Agene O. Sunday Portfolio',
+  tagline: 'Frontend Developer and Teacher',
+  // This is the name you see in your url
+  // https://res.cloudinary.com/<Your cloud name will be here>/image/upload/v1579118925/jp-blog-post-card.png
+  //https://res.cloudinary.com/ojaymedia/image/upload/v1593260165/website_card_Template.png
+  cloudName: 'ojaymedia',
+  imagePublicID: 'website_card_Template',
+  titleFont: 'Roboto',
+  titleExtraConfig: '_bold',
+  taglineFont: 'Roboto',
+})
